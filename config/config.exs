b9c93b56ffd99d913f979101ec3e99c6363bbb2b
Hitomaru Horino <type10tk@kahi-sv.info>
2020-10-31 02:52:59 +0900

    :wrench: LineBot configuration

--- config/config.exs
+++ config/config.exs
@@ -2,6 +2,12 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
+
+config :line_bot,
+  client_id: System.get_env("EMILLA_WATCHER_LINE_CLIENT_ID"),
+  client_secret: System.get_env("EMILLA_WATCHER_LINE_CLIENT_SECRET"),
+  skip_validation: false
+
 # This configuration is loaded before any dependency and is restricted
 # to this project. If another project depends on this project, this
 # file won't be loaded nor affect the parent project. For this reason,
