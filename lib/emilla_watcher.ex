da8f512dbef008e904f39cd9d38be52ee4fa0ff4
Hitomaru Horino <type10tk@kahi-sv.info>
2020-10-31 02:53:33 +0900

    :sparkles: Experimental bot functions

--- lib/emilla_watcher.ex
+++ lib/emilla_watcher.ex
@@ -1,18 +1,58 @@
 defmodule EmillaWatcher do
+  use LineBot
   @moduledoc """
   Documentation for EmillaWatcher.
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> EmillaWatcher.hello()
-      :world
-
+  メッセージのハンドリングを行う。
   """
-  def hello do
-    :world
+  def handle_message(%{"type" => "text", "text" => message}, _info, reply_token) do
+    reply_token
+    |> LineBot.send_reply(
+      %LineBot.Message.Text{text: message}
+    )
+  end
+  def handle_message(%{"type" => "sticker"}, _info, reply_token) do
+    reply_token
+    |> LineBot.send_reply(
+      %LineBot.Message.Text{text: "申し訳ありません。スタンプはプロトコル上対応していないのです。"}
+    )
+  end
+  def handle_message(%{"type" => "image"}, _info, reply_token) do
+    reply_token
+    |> LineBot.send_reply(
+      %LineBot.Message.Text{text: "申し訳ありません。画像はプロトコル上対応していないのです。"}
+    )
+  end
+  def handle_message(%{"type" => "video"}, _info, reply_token) do
+    reply_token
+    |> LineBot.send_reply(
+      %LineBot.Message.Text{text: "申し訳ありません。動画はプロトコル上対応していないのです。"}
+    )
+  end
+  def handle_message(%{"type" => "audio"}, _info, reply_token) do
+    reply_token
+    |> LineBot.send_reply(
+      %LineBot.Message.Text{text: "申し訳ありません。音声データはプロトコル上対応していないのです。"}
+    )
+  end
+  def handle_message(%{"type" => "file"}, _info, reply_token) do
+    reply_token
+    |> LineBot.send_reply(
+      %LineBot.Message.Text{text: "申し訳ありません。一般ファイルはプロトコル上対応していないのです。"}
+    )
+  end
+  def handle_message(%{"type" => "location"}, _info, reply_token) do
+    reply_token
+    |> LineBot.send_reply(
+      %LineBot.Message.Text{text: "申し訳ありません。位置情報はプロトコル上対応していないのです。"}
+    )
+  end
+  def handle_message(_message, _info, reply_token) do
+    reply_token
+    |> LineBot.send_reply(
+      %LineBot.Message.Text{text: "申し訳ありません。送信頂いた情報が識別できません。"}
+    )
   end
 end
